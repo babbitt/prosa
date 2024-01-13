eeaa9696952fab6952327ee7c534ed71efea09ee
babbitt <jrebbabbitt@gmail.com>
2024-01-13 16:32:01 -0500

    fix failing tests due to changes in codebase

--- test/spec/views/metadata.js
+++ test/spec/views/metadata.js
@@ -139,7 +139,7 @@ describe('Metadata editor view', function() {
         }
       }]);
       metadataEditor.render();
-      var $meta = $('#meta').find('input[type="text"]');
+      var $meta = $('#meta').find('input[type="number"]');
       expect($meta.length).to.equal(1);
       expect($meta.val()).to.equal('4');
       expect($meta.data('type')).to.equal('number');
@@ -286,7 +286,7 @@ describe('Metadata editor view', function() {
       expect($urlinput.val()).to.equal(newimage);
       $insert.click();
       var $meta = $('#meta').find('input[name="image"]');
-      expect($meta.val()).to.equal("{{site.baseurl}}/" + newimage);
+      expect($meta.val()).to.equal(newimage);
     });
 
     it('creates a text element if no field object exists', function() {
